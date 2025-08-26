8ad9e482dcc7cb001de6b02b6ed3a7b1e346a163
Ahmad RD <ahmaddahuri212@gmail.com>
2025-08-26 20:34:13 +0700

    Studi Kasus Coroutine Flow pada Aplikasi Tidak terdapat bug stuck loading saat  pertama kali get data seperti saat reactive menggunakan rxJava

--- app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
@@ -1,23 +1,22 @@
 package com.dicoding.tourismapp.core.data
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.map
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
 import com.dicoding.tourismapp.core.data.source.local.LocalDataSource
-import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
 import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.DataMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 class TourismRepository private constructor(
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource,
     private val appExecutors: AppExecutors
-): ITourismRepository {
+) : ITourismRepository {
 
     companion object {
         @Volatile
@@ -33,35 +32,36 @@ class TourismRepository private constructor(
             }
     }
 
-    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> =
-        object : NetworkBoundResource<List<Tourism>, List<TourismResponse>>(appExecutors) {
-            override fun loadFromDB(): LiveData<List<Tourism>> {
-                val source: LiveData<List<TourismEntity>> = localDataSource.getAllTourism()
-                val result = MediatorLiveData<List<Tourism>>()
-
-                result.addSource(source) { entities ->
-                    // manual konversi (helper singkat bisa dilihat di fun getFavoriteTourism)
-                    result.value = DataMapper.mapEntitiesToDomain(entities)
+    override fun getAllTourism(): Flow<Resource<List<Tourism>>> =
+        object : NetworkBoundResource<List<Tourism>, List<TourismResponse>>() {
+            override fun loadFromDB(): Flow<List<Tourism>> {
+                /*  Menccoba manual daripada menggunakan helper mtehod map
+                untuk versi kode singkat ada pada fungsi getFavorite    */
+                return flow {
+                    // ambil flow dari local data source
+                    localDataSource.getAllTourism().collect { entities ->
+                        // lakukan manual mapping sebelum diemit
+                        emit(DataMapper.mapEntitiesToDomain(entities))
+                    }
                 }
-                return result
             }
 
-            override fun shouldFetch(data: List<Tourism>?): Boolean =
-                data.isNullOrEmpty()
 
-            override fun createCall(): LiveData<ApiResponse<List<TourismResponse>>> =
+            override fun shouldFetch(data: List<Tourism>?): Boolean =
+                data == null || data.isEmpty()
+
+            override suspend fun createCall(): Flow<ApiResponse<List<TourismResponse>>> =
                 remoteDataSource.getAllTourism()
 
-            override fun saveCallResult(data: List<TourismResponse>) {
+            override suspend fun saveCallResult(data: List<TourismResponse>) {
                 val tourismList = DataMapper.mapResponsesToEntities(data)
                 localDataSource.insertTourism(tourismList)
             }
-        }.asLiveData()
+        }.asFlow()
 
-    override fun getFavoriteTourism(): LiveData<List<Tourism>> {
-        return localDataSource.getFavoriteTourism().map {
-            DataMapper.mapEntitiesToDomainManual(it)
-        }
+    override fun getFavoriteTourism(): Flow<List<Tourism>> {
+        return localDataSource.getFavoriteTourism().map { DataMapper.mapEntitiesToDomain(it) }
+
     }
 
     override fun setFavoriteTourism(tourism: Tourism, state: Boolean) {
